181bf5b4a63e50835e0a60d537c11271ac893417
unknown <shahinbanu.bodeliwala.at@gmail.com>
2023-11-15 18:22:03 +0530

    product by category

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,13 +1,17 @@
+import 'package:assignment10/data/models/category/category_model.dart';
 import 'package:assignment10/data/models/product/product_model.dart';
+import 'package:assignment10/logic/cubits/category_product_cubit.dart/category_product_cubit.dart';
 import 'package:assignment10/presentation/screens/auth/login_screen.dart';
 import 'package:assignment10/presentation/screens/auth/providers/login_provider.dart';
 import 'package:assignment10/presentation/screens/auth/providers/signup_provider.dart';
 import 'package:assignment10/presentation/screens/auth/signup_screen.dart';
 import 'package:assignment10/presentation/screens/cart/cart_screen.dart';
 import 'package:assignment10/presentation/screens/home/home_screen.dart';
+import 'package:assignment10/presentation/screens/product/category_product_screen.dart';
 import 'package:assignment10/presentation/screens/product/product_details_screen.dart';
 import 'package:assignment10/presentation/screens/splash/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class Routes {
@@ -42,6 +46,14 @@ class Routes {
       case CartScreen.routeName:
         return CupertinoPageRoute(
           builder: (context) => const CartScreen(),
+        );
+      case CategoryProductScreen.routeName:
+        return CupertinoPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) =>
+                CategoryProductCubit(settings.arguments as CategoryModel),
+            child: const CategoryProductScreen(),
+          ),
         );
 
       default:
